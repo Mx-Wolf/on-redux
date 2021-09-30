22d1caa6a664f53bfab9edaa1cafd6f6308ab28f
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-09-30 17:50:43 +0300

    adds state usage in place call button

--- src/components/place-call/place-call.tsx
+++ src/components/place-call/place-call.tsx
@@ -1,7 +1,17 @@
+import classNames from "classnames";
 import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
-export const PlaceCall:FC = ()=>{
-  return (<div id="call">
-  <i className="fa fa-phone" aria-hidden="true" />
-</div>);
+import classes from "./place-call.module.scss";
+
+export const PlaceCall: FC = () => {
+  const disabled = useSelector((state: RootState) => state.display.length !== 10);
+  return (
+    <div
+      className={classNames({ [classes["disabled"]]: disabled }, "call")}
+    >
+      <i className="fa fa-phone" aria-hidden="true" />
+    </div>
+  );
 }

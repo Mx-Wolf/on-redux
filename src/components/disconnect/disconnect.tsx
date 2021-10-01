73c4dc46de0e90c85ba167a2548c3d266ab5b306
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-10-01 16:39:28 +0300

    adds disconnect ui

--- src/components/disconnect/disconnect.tsx
+++ src/components/disconnect/disconnect.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react"
+import { useDispatch } from "react-redux";
 import { useEngagedRule } from "../../store/hooks/use-engaged-rule"
+import { changed } from "../../store/slices/line";
 
-export const Disonnect:FC=()=>{
+export const Disonnect: FC = () => {
   const engaged = useEngagedRule();
+  const dispatch = useDispatch();
   return (
-    <button className="disconnect" disabled={!engaged}>
-      <i className="fa fa-chain-broken" aria-hidden="true"/>      
+    <button
+      className="disconnect"
+      disabled={!engaged}
+      onClick={() => dispatch(changed("online"))}
+    >
+      <i
+        className="fa fa-chain-broken"
+        aria-hidden="true"
+      />
     </button>
   )
 }

37f0535e3768c82bea32147e50c7dfe6d757e70d
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-10-01 15:12:56 +0300

    adds the contact component

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { usePicsumId } from "../../store/hooks/use-picsum-id";
+import { ContactInfo } from "./contact-info";
 import { ContactPlaceholder } from "./contact-placeholder";
 
 export const Contact: FC = () => {
